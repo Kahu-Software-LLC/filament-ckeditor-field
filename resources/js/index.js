a1f49eb52fc6c4db9e892c75fb7b9d038e051c19
tominal <tom@thomasj.me>
2024-09-26 23:40:22 -0500

    Test ClassicEditor

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,7 @@
-import { ClassicEditor, Essentials, Bold, Italic, Font, Paragraph } from 'ckeditor5';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+if (typeof window !== 'undefined') {
+  window.ClassicEditor = ClassicEditor;
+}
 
 import 'ckeditor5/ckeditor5.css';
-
-window.ClassicEditor = ClassicEditor;
