30c0f8c988b3864144b0bc786587221c8aef91f4
tominal <tom@thomasj.me>
2024-09-26 20:45:48 -0500

    Update ckeditor.blade.php

--- resources/views/ckeditor.blade.php
+++ resources/views/ckeditor.blade.php
@@ -1,6 +1,6 @@
 <div
     x-data
-    x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptHref('filament-ckeditor-field', package: 'kahusoftware/filament-ckeditor-field'))]"
+    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-ckeditor-field', package: 'kahusoftware/filament-ckeditor-field'))]"
 >
     <textarea id="ckeditor" name="ckeditor">{{ $getContent() }}</textarea>
 </div>
